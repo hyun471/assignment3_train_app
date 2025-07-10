587212ddc3fda49c6a818832817ba3efdc155e36
hyun471 <khs101ttl@gmail.com>
2025-07-10 11:05:27 +0900

    first commit

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('기차 예매')),
+      ),
+      backgroundColor: Colors.grey[200],
+      body: Column(),
+    );
+  }
+}
